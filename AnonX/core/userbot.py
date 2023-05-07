a8d9199b52f3e9ef9077d19761f1a0a3170eaf85
Rishii7205 <118364756+Rishii7205@users.noreply.github.com>
2023-05-07 16:51:19 +0530

    Update userbot.py

--- AnonX/core/userbot.py
+++ AnonX/core/userbot.py
@@ -48,8 +48,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("FallenAssociation")
+                await self.one.join_chat("loveguruoo")
+                await self.one.join_chat("innocentop")
             except:
                 pass
             assistants.append(1)
